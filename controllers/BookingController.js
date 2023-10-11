3ce82f0e88d9df23cace856c41b26f181530648e
PhucLoc <phuclocub@gmail.com>
2023-10-12 01:55:23 +0700

    complete%

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -1,142 +1,172 @@
-window.BookingController = function ($scope, $http,$location) {
+window.BookingController = function ($scope, $http, $location) {
     $scope.title = "This is booing page";
     const apiStudents = "http://localhost:3000/bookingRequest";
     var countNullError = 0;
-    var submitCount =0;
-    $scope.fullNameCheck="*";
-    $scope.idCheck="*";
-    $scope.emailCheck="*";
-    $scope.phoneCheck="*";
-    $scope.flightCheck="*";
-    $scope.dobCheck="*";
-    $scope.luggageCheck="*";
-    $scope.paymentCheck="*"; 
-    function checkNull(x){
-        countNullError=0
-        try{
-            if(x.fullName){
-                $scope.fullNameCheck="*";
+    var submitCount = 0;
+    // $scope.fullNameCheck="*";
+    // $scope.idCheck="*";
+    // $scope.emailCheck="*";
+    // $scope.phoneCheck="*";
+    // $scope.flightCheck="*";
+    // $scope.dobCheck="*";
+    // $scope.luggageCheck="*";
+    // $scope.paymentCheck="*"; 
+
+    const validateEmail = (email) => {
+        return email.match(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    };
+    function isVietnamesePhoneNumber(number) {
+        return /(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/.test(number);
+    }
+    function validateIDCardNo(idCardNo) {
+        return /([0-9]{12})\b/.test(idCardNo);
+    }
+    function validate(x) {
+        countNullError = 0
+        try {
+            if (x.fullName) {
+                $scope.fullNameCheck = "";
             }
-            if(x.fullName.trim() === ""){
+            if (x.fullName.trim() === "") {
                 countNullError++
-                $scope.fullNameCheck="*Dont empty full name field";
+                $scope.fullNameCheck = "*Dont empty full name field";
             }
         }
-        catch(err){
+        catch (err) {
             countNullError++
-            $scope.fullNameCheck="*Dont empty full name field";
+            $scope.fullNameCheck = "*Dont empty full name field";
         }
-        try{
-            if(x.id){
-                $scope.idCheck="*";
+        try {
+            if (x.idNo) {
+                if(!validateIDCardNo(x.idNo)){
+                    countNullError++;
+                    $scope.idCheck = "*Identification number is not correct";
+                }
+                else{
+                    $scope.idCheck = "";
+                }
             }
-            if(x.id.trim() === ""){
+            if (x.idNo.trim() === "") {
                 countNullError++
-                $scope.idCheck="*Dont empty idCard field";
+                $scope.idCheck = "*Dont empty idCard field";
             }
         }
-        catch(err){
+        catch (err) {
             countNullError++
-            $scope.idCheck="*Dont empty idCard field";
+            $scope.idCheck = "*Dont empty idCard field";
         }
-        try{
-            if(x.email){
-                $scope.emailCheck="*";
+        try {
+            if (x.email) {
+                if (validateEmail(x.email) === null) {
+                    countNullError++;
+                    $scope.emailCheck = "Email is not correct";
+                }
+                else {
+                    $scope.emailCheck = "";
+                }
+
             }
-            if(x.email.trim() === ""){
+            if (x.email.trim() === "") {
                 countNullError++
-                $scope.emailCheck="*Dont empty email field";
+                $scope.emailCheck = "*Dont empty email field";
             }
         }
-        catch(err){
+        catch (err) {
             countNullError++
-            $scope.emailCheck="*Dont empty email field";
+            $scope.emailCheck = "*Dont empty email field";
         }
-        try{
-            if(x.phonenumber){
-                $scope.phoneCheck="*";
+        try {
+            if (x.phonenumber) {
+                if(!isVietnamesePhoneNumber(x.phonenumber)){
+                    countNullError++;
+                    $scope.phoneCheck = "phone number is not correct";
+                }
+                else{
+                    $scope.phoneCheck = "";
+                }
             }
-            if(x.phonenumber.trim() === ""){
+            if (x.phonenumber.trim() === "") {
                 countNullError++
-                $scope.phoneCheck="*Dont empty phone number";
+                $scope.phoneCheck = "*Dont empty phone number";
             }
         }
-        catch(err){
+        catch (err) {
             countNullError++
-            $scope.phoneCheck="*Dont empty phone number";
+            $scope.phoneCheck = "*Dont empty phone number";
         }
-        try{
-            if(x.flight){
-                $scope.flightCheck="*";
+        try {
+            if (x.flight) {
+                $scope.flightCheck = "";
             }
-            if(x.flight.trim() === ""){
+            if (x.flight.trim() === "") {
                 countNullError++
-                $scope.flightCheck="*Dont empty flight field";
+                $scope.flightCheck = "*Dont empty flight field";
             }
         }
-        catch(err){
+        catch (err) {
             countNullError++
-            $scope.flightCheck="*Dont empty flight field";
+            $scope.flightCheck = "*Dont empty flight field";
         }
-        try{
-            if(x.dob){
-                $scope.dobCheck="*";
+        try {
+            if (x.dob) {
+                $scope.dobCheck = "";
             }
-            if(isNaN(x.dob)){
+            if (isNaN(x.dob)) {
                 countNullError++
-                $scope.dobCheck="*Dont empty dob field";
+                $scope.dobCheck = "*Dont empty dob field";
             }
         }
-        catch(err){
+        catch (err) {
             countNullError++
-            $scope.dobCheck="*Dont empty dob field";
+            $scope.dobCheck = "*Dont empty dob field";
         }
-        try{
-            if(x.luggage){
-                $scope.luggageCheck="*"
+        try {
+            if (x.luggage) {
+                $scope.luggageCheck = ""
             }
-            if(x.luggage.trim() === ""){
+            if (x.luggage.trim() === "") {
                 countNullError++
-                $scope.luggageCheck="*Dont empty luggage field"
+                $scope.luggageCheck = "*Dont empty luggage field"
             }
         }
-        catch(err){
+        catch (err) {
             countNullError++
-            $scope.luggageCheck="*Dont empty luggage field"
+            $scope.luggageCheck = "*Dont empty luggage field"
         }
-        try{
-            if(x.payment){
-                $scope.paymentCheck="*"; 
+        try {
+            if (x.payment) {
+                $scope.paymentCheck = "";
             }
-            if(x.payment.trim() === ""){
+            if (x.payment.trim() === "") {
                 countNullError++
-                $scope.paymentCheck="*Dont empty payment field"; 
+                $scope.paymentCheck = "*Dont empty payment field";
             }
         }
-        catch(err){
+        catch (err) {
             countNullError++
-            $scope.paymentCheck="*Dont empty payment field"; 
+            $scope.paymentCheck = "*Dont empty payment field";
         }
-        console.log("count error:"+countNullError)
-        if(countNullError > 0){
+        console.log("count error:" + countNullError)
+        if (countNullError > 0) {
             return 1;
-        } 
-        else{
+        }
+        else {
             return 0;
         }
     }
 
-    function showMessage(){
+    function showMessage() {
     }
     $scope.submit = function () {
-        if(checkNull($scope.customer)===0){
+        
+        if (validate($scope.customer) === 0) {
             console.log("do post");
-             $http.post((apiStudents),$scope.customer).then(function(respone){$location.path('/resultBooking')})
+            $http.post((apiStudents), $scope.customer).then(function (respone) { $location.path('/resultBooking') })
         }
-        else{
+        else {
             console.log("do nothing");
-            //do fill out validate
-            
         }
     }
 }
--- controllers/ResultBookingController.js
+++ controllers/ResultBookingController.js
@@ -3,11 +3,12 @@ window.ResultBookingController = function($scope,$http){
     const apiStudents = "http://localhost:3000/bookingRequest";
     $http.get(apiStudents).then(function (response) {
         var obj0 = response.data[response.data.length-1];
+        // var date = new Date(obj0.dob);
         $scope.fullname = obj0.fullName;
-        $scope.id = obj0.ID;
+        $scope.id = obj0.idNo;
         $scope.email = obj0.email;
         $scope.gender = obj0.gender;
-        $scope.phone = obj0.phone;
+        $scope.phone = obj0.phonenumber;
         $scope.flight = obj0.flight;
         $scope.dob = obj0.dob;
         $scope.luggage = obj0.luggage;
